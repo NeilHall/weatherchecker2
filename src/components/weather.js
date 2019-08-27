d2711ef367e87ca89f6e34293f26ed43abcd8dbe
Neil Hall <neil@thingsyettobe.co.uk>
2019-08-28 00:26:58 +0100

    minor tweak

--- src/components/weather.js
+++ src/components/weather.js
@@ -2,8 +2,7 @@
 import React, {useState, } from 'react';
 
 const GetWeatherForLocation = async (latitude, longitude) => {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat='
-                +latitude+'&lon='+longitude+'&appid=APPID&units=metric&mode=json')
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=APP_ID&units=metric&mode=json')
     let data = await response.json();
     // console.log(data);
     return data;
